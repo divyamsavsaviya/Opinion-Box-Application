4c96468ca57d1c32262ca17dcd15ff26cf5c3e39
divyamsavsaviya <savsaviyadivyam364@gmail.com>
2020-10-24 23:01:36 +0530

    Opinion Box

--- app/src/main/java/com/example/finalopinionbox/EventMainActivity.java
+++ app/src/main/java/com/example/finalopinionbox/EventMainActivity.java
@@ -3,12 +3,44 @@ package com.example.finalopinionbox;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class EventMainActivity extends AppCompatActivity {
+
+    RecyclerView recview;
+    myAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_event_main);
+
+        recview = (RecyclerView) findViewById(R.id.recview);
+        recview.setLayoutManager(new LinearLayoutManager(this));
+
+        FirebaseRecyclerOptions<model> options =
+                new FirebaseRecyclerOptions.Builder<model>()
+                        .setQuery(FirebaseDatabase.getInstance().getReference().child("Event"), model.class)
+                        .build();
+
+        adapter = new myAdapter(options);
+        recview.setAdapter(adapter);
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        adapter.startListening();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        adapter.stopListening();
     }
 }
